ae47aad0537f1e87e1f9a7442ea0206142212f98
= <thewillmoss@yahoo.com>
2020-04-28 12:51:04 -0500

    Updates

---
+++ precache-manifest.5f53d49f71b61f328c2d95119f46a81b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3c38f43cdbdcd961ff15693bcb6064",
+    "url": "/wordgame/index.html"
+  },
+  {
+    "revision": "345ceefa8bf864c25197",
+    "url": "/wordgame/static/css/main.c787aec4.chunk.css"
+  },
+  {
+    "revision": "e313a673e7388d7ac9ac",
+    "url": "/wordgame/static/js/2.1308a2ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wordgame/static/js/2.1308a2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345ceefa8bf864c25197",
+    "url": "/wordgame/static/js/main.08292f78.chunk.js"
+  },
+  {
+    "revision": "2631ff3ea8d072df0e73",
+    "url": "/wordgame/static/js/runtime-main.6b980d44.js"
+  }
+]);
